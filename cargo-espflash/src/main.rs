2869852e5fdae162746c22fb31f531a68542661a
Jesse Braham <jesse@beta7.io>
2021-09-03 11:52:43 -0700

    Rewrite cargo-espflash to use clap instead of pico-args

--- cargo-espflash/src/main.rs
+++ cargo-espflash/src/main.rs
@@ -1,150 +1,180 @@
-mod cargo_config;
-
-use std::ffi::OsString;
-use std::fs::read;
-use std::path::PathBuf;
-use std::process::{exit, Command, ExitStatus, Stdio};
-
-use crate::cargo_config::has_build_std;
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{anyhow, bail, Context};
 use cargo_metadata::Message;
+use clap::{App, Arg, SubCommand};
 use espflash::{Config, Flasher};
-use pico_args::Arguments;
 use serial::{BaudRate, SerialPort};
 
-fn main() -> Result<()> {
-    let args = parse_args().expect("Unable to parse command-line arguments");
-    let config = Config::load();
+use std::{
+    fs::read,
+    path::PathBuf,
+    process::{exit, Command, ExitStatus, Stdio},
+    string::ToString,
+};
 
-    if args.help || (args.serial.is_none() && config.connection.serial.is_none()) {
-        return usage();
-    }
+mod cargo_config;
+use cargo_config::has_build_std;
 
-    let port = args
-        .serial
-        .or(config.connection.serial)
-        .context("serial port missing")?;
+fn main() -> anyhow::Result<()> {
+    let mut app = App::new(env!("CARGO_PKG_NAME"))
+        .bin_name("cargo")
+        .subcommand(
+            SubCommand::with_name("espflash")
+                .version(env!("CARGO_PKG_VERSION"))
+                .about(env!("CARGO_PKG_DESCRIPTION"))
+                .arg(
+                    Arg::with_name("board_info")
+                        .long("board-info")
+                        .help("Display the connected board's information"),
+                )
+                .arg(
+                    Arg::with_name("ram")
+                        .long("ram")
+                        .help("Load the application to RAM instead of Flash"),
+                )
+                .arg(
+                    Arg::with_name("release")
+                        .long("release")
+                        .help("Build the application using the release profile"),
+                )
+                .arg(
+                    Arg::with_name("example")
+                        .long("example")
+                        .takes_value(true)
+                        .value_name("EXAMPLE")
+                        .help("Example to build and flash"),
+                )
+                .arg(
+                    Arg::with_name("features")
+                        .long("features")
+                        .use_delimiter(true)
+                        .takes_value(true)
+                        .value_name("FEATURES")
+                        .help("Comma delimited list of build features"),
+                )
+                .arg(
+                    Arg::with_name("speed")
+                        .long("speed")
+                        .takes_value(true)
+                        .value_name("SPEED")
+                        .help("Baud rate at which to flash target device"),
+                )
+                .arg(
+                    Arg::with_name("serial")
+                        .takes_value(true)
+                        .value_name("SERIAL")
+                        .help("Serial port connected to target device"),
+                ),
+        );
 
-    let speed = args.speed.map(|v| BaudRate::from_speed(v as usize));
-
-    // Don't build if we are just querying board info
-    let path = if !args.board_info {
-        build(args.release, &args.example, &args.features)?
-    } else {
-        PathBuf::new()
+    let matches = app.clone().get_matches();
+    let matches = match matches.subcommand_matches("espflash") {
+        Some(matches) => matches,
+        None => {
+            app.print_help()?;
+            exit(0);
+        }
     };
 
+    let config = Config::load();
+
+    // The serial port must be specified, either as a command-line argument or in
+    // the cargo configuration file. In the case that both have been provided the
+    // command-line argument will take precedence.
+    let port = if let Some(serial) = matches.value_of("serial") {
+        serial.to_string()
+    } else if let Some(serial) = config.connection.serial {
+        serial
+    } else {
+        app.print_help()?;
+        exit(0);
+    };
+
+    // Only build the application if the '--board-info' flag has not been passed.
+    let show_board_info = matches.is_present("board_info");
+    let path = if !show_board_info {
+        let release = matches.is_present("release");
+        let example = matches.value_of("example");
+        let features = matches.value_of("features");
+
+        let path = build(release, example, features)?;
+
+        Some(path)
+    } else {
+        None
+    };
+
+    // Attempt to open the serial port and set its initial baud rate.
+    println!("Serial port: {}", port);
+    println!("Connecting...\n");
     let mut serial = serial::open(&port).context(format!("Failed to open serial port {}", port))?;
     serial.reconfigure(&|settings| {
         settings.set_baud_rate(BaudRate::Baud115200)?;
         Ok(())
     })?;
 
+    // Parse the baud rate if provided as as a command-line argument.
+    let speed = if let Some(speed) = matches.value_of("speed") {
+        let speed = speed.parse::<usize>()?;
+        Some(BaudRate::from_speed(speed))
+    } else {
+        None
+    };
+
+    // Connect the Flasher to the target device. If the '--board-info' flag has been
+    // provided, display the board info and terminate the application.
     let mut flasher = Flasher::connect(serial, speed)?;
-    if args.board_info {
-        return board_info(&flasher);
+    if show_board_info {
+        board_info(&flasher);
+        return Ok(());
     }
 
-    let elf_data = read(&path)?;
-
-    if args.ram {
+    // Read the ELF data from the build path and load it to the target.
+    let elf_data = read(path.unwrap())?;
+    if matches.is_present("ram") {
         flasher.load_elf_to_ram(&elf_data)?;
     } else {
         flasher.load_elf_to_flash(&elf_data)?;
     }
 
+    // We're all done!
     Ok(())
 }
 
-#[derive(Debug)]
-struct AppArgs {
-    help: bool,
-    board_info: bool,
-    ram: bool,
-    release: bool,
-    example: Option<String>,
-    features: Option<String>,
-    chip: Option<String>,
-    speed: Option<u32>,
-    serial: Option<String>,
+fn board_info(flasher: &Flasher) {
+    println!("Chip type:  {}", flasher.chip());
+    println!("Flash size: {}", flasher.flash_size());
 }
 
-#[allow(clippy::unnecessary_wraps)]
-fn usage() -> Result<()> {
-    let usage = "Usage: cargo espflash \
-      [--board-info] \
-      [--ram] \
-      [--release] \
-      [--example EXAMPLE] \
-      [--chip {{esp32,esp32c3,esp8266}}] \
-      [--speed BAUD] \
-      <serial>";
-
-    println!("{}", usage);
-
-    Ok(())
-}
-
-#[allow(clippy::unnecessary_wraps)]
-fn board_info(flasher: &Flasher) -> Result<()> {
-    println!("Chip type:  {:?}", flasher.chip());
-    println!("Flash size: {:?}", flasher.flash_size());
-
-    Ok(())
-}
-
-fn parse_args() -> Result<AppArgs> {
-    // Skip the command and subcommand (ie. 'cargo espflash') and convert the
-    // remaining arguments to the expected type.
-    let args = std::env::args().skip(2).map(OsString::from).collect();
-
-    let mut args = Arguments::from_vec(args);
-
-    let app_args = AppArgs {
-        help: args.contains("--help"),
-        board_info: args.contains("--board-info"),
-        ram: args.contains("--ram"),
-        release: args.contains("--release"),
-        example: args.opt_value_from_str("--example")?,
-        features: args.opt_value_from_str("--features")?,
-        chip: args.opt_value_from_str("--chip")?,
-        speed: args.opt_value_from_str("--speed")?,
-        serial: args.opt_free_from_str()?,
-    };
-
-    Ok(app_args)
-}
-
-fn build(release: bool, example: &Option<String>, features: &Option<String>) -> Result<PathBuf> {
-    let mut args: Vec<String> = vec![];
-
-    if release {
-        args.push("--release".to_string());
-    }
-
-    match example {
-        Some(example) => {
-            args.push("--example".to_string());
-            args.push(example.to_string());
-        }
-        None => {}
-    }
-
-    match features {
-        Some(features) => {
-            args.push("--features".to_string());
-            args.push(features.to_string());
-        }
-        None => {}
-    }
-
+fn build(release: bool, example: Option<&str>, features: Option<&str>) -> anyhow::Result<PathBuf> {
+    // The 'build-std' unstable cargo feature is required to enable
+    // cross-compilation. If it has not been set then we cannot build the
+    // application.
     if !has_build_std(".") {
         bail!(
-            r#"cargo currently requires the unstable build-std, ensure .cargo/config{{.toml}} has the appropriate options.
-        See: https://doc.rust-lang.org/cargo/reference/unstable.html#build-std"#
+            "cargo currently requires the unstable 'build-std' feature, ensure \
+            that .cargo/config{.toml} has the appropriate options.\n  \
+            See: https://doc.rust-lang.org/cargo/reference/unstable.html#build-std"
         );
     };
 
+    // Build the list of arguments to pass to 'cargo build'.
+    let mut args = vec![];
+
+    if release {
+        args.push("--release");
+    }
+
+    if let Some(example) = example {
+        args.push("--example");
+        args.push(example);
+    }
+
+    if let Some(features) = features {
+        args.push("--features");
+        args.push(features);
+    }
+
+    // Invoke the 'cargo build' command, passing our list of arguments.
     let output = Command::new("cargo")
         .arg("build")
         .args(args)
@@ -165,8 +195,7 @@ fn build(release: bool, example: &Option<String>, features: &Option<String>) -> 
             Message::CompilerArtifact(artifact) => {
                 if artifact.executable.is_some() {
                     if target_artifact.is_some() {
-                        // We found multiple binary artifacts,
-                        // so we don't know which one to use.
+                        // We found multiple binary artifacts, so we don't know which one to use.
                         bail!("Multiple artifacts found, please specify one with --bin");
                     } else {
                         target_artifact = Some(artifact);
@@ -178,29 +207,31 @@ fn build(release: bool, example: &Option<String>, features: &Option<String>) -> 
                     print!("{}", rendered);
                 }
             }
-            // Ignore other messages.
+            // Ignore all other messages.
             _ => (),
         }
     }
 
-    // Check if the command succeeded, otherwise return an error.
-    // Any error messages occuring during the build are shown above,
-    // when the compiler messages are rendered.
+    // Check if the command succeeded, otherwise return an error. Any error messages
+    // occuring during the build are shown above, when the compiler messages are
+    // rendered.
     if !output.status.success() {
         exit_with_process_status(output.status);
     }
 
-    if let Some(artifact) = target_artifact {
-        let artifact_path = PathBuf::from(
-            artifact
-                .executable
-                .ok_or(anyhow!("artifact executable path is missing"))?
-                .as_path(),
-        );
-        Ok(artifact_path)
-    } else {
+    // If no target artifact was found, we don't have a path to return.
+    if target_artifact.is_none() {
         bail!("Artifact not found");
     }
+
+    let artifact_path = PathBuf::from(
+        target_artifact
+            .unwrap()
+            .executable
+            .ok_or_else(|| anyhow!("artifact executable path is missing"))?,
+    );
+
+    Ok(artifact_path)
 }
 
 #[cfg(unix)]
