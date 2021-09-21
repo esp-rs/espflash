6d3833abccb18c7ba3dd5f26fdf8d13073bbfcda
Robin Appelman <robin@icewind.nl>
2021-09-21 23:21:08 +0200

    add partition table and bootloader option to plain espflash

--- espflash/src/main.rs
+++ espflash/src/main.rs
@@ -1,13 +1,13 @@
-use std::fs::read;
+use std::fs::{read, read_to_string};
 
-use espflash::{Config, Error, Flasher};
+use espflash::{Config, Error, Flasher, PartitionTable};
 use miette::{IntoDiagnostic, Result, WrapErr};
 use pico_args::Arguments;
 use serial::{BaudRate, FlowControl, SerialPort};
 
 #[allow(clippy::unnecessary_wraps)]
 fn help() -> Result<()> {
-    println!("Usage: espflash [--board-info] [--ram] <serial> <elf image>");
+    println!("Usage: espflash [--board-info] [--ram] [--partition-table partition.csv] [--bootloader boot.bin] <serial> <elf image>");
     Ok(())
 }
 
@@ -21,6 +21,12 @@ fn main() -> Result<()> {
 
     let ram = args.contains("--ram");
     let board_info = args.contains("--board-info");
+    let bootloader_path = args
+        .opt_value_from_str::<_, String>("--bootloader")
+        .into_diagnostic()?;
+    let partition_table_path = args
+        .opt_value_from_str::<_, String>("--partition-table")
+        .into_diagnostic()?;
 
     let mut serial: Option<String> = args.opt_free_from_str().into_diagnostic()?;
     let mut elf: Option<String> = args.opt_free_from_str().into_diagnostic()?;
@@ -67,7 +73,32 @@ fn main() -> Result<()> {
     if ram {
         flasher.load_elf_to_ram(&input_bytes)?;
     } else {
-        flasher.load_elf_to_flash(&input_bytes, None, None)?;
+        let bootloader = bootloader_path
+            .as_deref()
+            .map(read)
+            .transpose()
+            .into_diagnostic()
+            .wrap_err_with(|| {
+                format!(
+                    "Failed to open bootloader image \"{}\"",
+                    bootloader_path.unwrap()
+                )
+            })?;
+        let partition_table = partition_table_path
+            .as_deref()
+            .map(|path| {
+                let table = read_to_string(path)?;
+                PartitionTable::try_from_str(&table).map_err(Error::from)
+            })
+            .transpose()
+            .into_diagnostic()
+            .wrap_err_with(|| {
+                format!(
+                    "Failed to load partition table \"{}\"",
+                    partition_table_path.unwrap()
+                )
+            })?;
+        flasher.load_elf_to_flash(&input_bytes, bootloader, partition_table)?;
     }
 
     Ok(())
