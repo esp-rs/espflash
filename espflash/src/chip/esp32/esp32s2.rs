413264eea49e7fd41ecf0469ef508955879396c1
Jesse Braham <jesse@beta7.io>
2022-03-24 10:12:05 -0700

    Reduce repetition when defining chips

--- espflash/src/chip/esp32/esp32s2.rs
+++ espflash/src/chip/esp32/esp32s2.rs
@@ -10,29 +10,18 @@ use crate::{
     Chip, Error, PartitionTable,
 };
 
+pub const PARAMS: Esp32Params = Esp32Params::new(
+    0x1000,
+    0x10000,
+    0x100000,
+    2,
+    include_bytes!("../../../bootloader/esp32s2-bootloader.bin"),
+);
+
 pub struct Esp32s2;
 
-const IROM_MAP_START: u32 = 0x40080000;
-const IROM_MAP_END: u32 = 0x40b80000;
-
-const DROM_MAP_START: u32 = 0x3F000000;
-const DROM_MAP_END: u32 = 0x3F3F0000;
-
-pub const PARAMS: Esp32Params = Esp32Params {
-    boot_addr: 0x1000,
-    partition_addr: 0x8000,
-    nvs_addr: 0x9000,
-    nvs_size: 0x6000,
-    phy_init_data_addr: 0xf000,
-    phy_init_data_size: 0x1000,
-    app_addr: 0x10000,
-    app_size: 0x100000,
-    chip_id: 2,
-    default_bootloader: include_bytes!("../../../bootloader/esp32s2-bootloader.bin"),
-};
-
 impl ChipType for Esp32s2 {
-    const CHIP_DETECT_MAGIC_VALUE: u32 = 0x000007c6;
+    const CHIP_DETECT_MAGIC_VALUES: &'static [u32] = &[0x000007c6];
 
     const UART_CLKDIV_REG: u32 = 0x3f400014;
 
@@ -46,11 +35,10 @@ impl ChipType for Esp32s2 {
         miso_length_offset: Some(0x28),
     };
 
-    const FLASH_RANGES: &'static [Range<u32>] =
-        &[IROM_MAP_START..IROM_MAP_END, DROM_MAP_START..DROM_MAP_END];
-
-    const DEFAULT_IMAGE_FORMAT: ImageFormatId = ImageFormatId::Bootloader;
-    const SUPPORTED_IMAGE_FORMATS: &'static [ImageFormatId] = &[ImageFormatId::Bootloader];
+    const FLASH_RANGES: &'static [Range<u32>] = &[
+        0x40080000..0x40b80000, // IROM
+        0x3F000000..0x3F3F0000, // DROM
+    ];
 
     const SUPPORTED_TARGETS: &'static [&'static str] =
         &["xtensa-esp32s2-none-elf", "xtensa-esp32s2-espidf"];
