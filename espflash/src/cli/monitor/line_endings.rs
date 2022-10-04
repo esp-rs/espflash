0720a44136a3a2a9ab6416759202b3eeaa1e849d
Jesse Braham <jesse@beta7.io>
2022-10-04 14:29:04 -0700

    Remove the `espmonitor` dependency, re-implement our own monitor
    Unfortunately since `espmonitor` is licensed under GPL we are no longer able to use it after the relicensing

---
+++ espflash/src/cli/monitor/line_endings.rs
@@ -0,0 +1,60 @@
+// Adapted from: https://github.com/derekdreery/normalize-line-endings
+
+struct Normalized<I> {
+    iter: I,
+    prev_was_cr: bool,
+    peeked: Option<u8>,
+}
+
+impl<I> Iterator for Normalized<I>
+where
+    I: Iterator<Item = u8>,
+{
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        if let Some(peeked) = self.peeked.take() {
+            return Some(peeked);
+        }
+
+        match self.iter.next() {
+            Some(b'\n') if !self.prev_was_cr => {
+                self.peeked = Some(b'\n');
+                self.prev_was_cr = false;
+                Some(b'\r')
+            }
+            Some(b'\r') => {
+                self.prev_was_cr = true;
+                Some(b'\r')
+            }
+            any => {
+                self.prev_was_cr = false;
+                any
+            }
+        }
+    }
+}
+
+pub(crate) fn normalized(iter: impl Iterator<Item = u8>) -> impl Iterator<Item = u8> {
+    Normalized {
+        iter,
+        prev_was_cr: false,
+        peeked: None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter::FromIterator;
+
+    use super::*;
+
+    #[test]
+    fn test_normalized() {
+        let input = b"This is a string \n with \n some \n\r\n random newlines\r\n\n";
+        assert_eq!(
+            &Vec::from_iter(normalized(input.iter().copied())),
+            b"This is a string \r\n with \r\n some \r\n\r\n random newlines\r\n\r\n"
+        );
+    }
+}
---
+++ espflash/src/cli/monitor/symbols.rs
@@ -0,0 +1,60 @@
+use std::error::Error;
+
+use addr2line::{
+    gimli::{EndianRcSlice, RunTimeEndian},
+    object::{read::File, Object},
+    Context,
+};
+
+pub(crate) struct Symbols<'sym> {
+    file: File<'sym, &'sym [u8]>,
+    ctx: Context<EndianRcSlice<RunTimeEndian>>,
+}
+
+impl<'sym> Symbols<'sym> {
+    pub fn try_from(bytes: &'sym [u8]) -> Result<Self, Box<dyn Error>> {
+        let file = File::parse(bytes)?;
+        let ctx = Context::new(&file)?;
+
+        Ok(Self { file, ctx })
+    }
+
+    pub fn get_name(&self, addr: u64) -> Option<String> {
+        // The basic steps here are:
+        //   1. find which frame `addr` is in
+        //   2. look up and demangle the function name
+        //   3. if no function name is found, try to look it up in the object file
+        //      directly
+        //   4. return a demangled function name, if one was found
+        self.ctx
+            .find_frames(addr)
+            .ok()
+            .and_then(|mut frames| {
+                frames.next().ok().flatten().and_then(|frame| {
+                    frame
+                        .function
+                        .and_then(|name| name.demangle().map(|s| s.into_owned()).ok())
+                })
+            })
+            .or_else(|| {
+                self.file
+                    .symbol_map()
+                    .get(addr)
+                    .map(|sym| sym.name().to_string())
+            })
+    }
+
+    pub fn get_location(&self, addr: u64) -> Option<(String, u32)> {
+        // Find the location which `addr` is in. If we can dedetermine a file name and
+        // line number for this function we will return them both in a tuple.
+        self.ctx.find_location(addr).ok()?.map(|location| {
+            let file = location.file.map(|f| f.to_string());
+            let line = location.line;
+
+            match (file, line) {
+                (Some(file), Some(line)) => Some((file, line)),
+                _ => None,
+            }
+        })?
+    }
+}
