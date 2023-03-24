62b882e07b8dc73b2aa1b27ad6105298d13f5475
Kirill Mikhailov <62840029+playfulFence@users.noreply.github.com>
2023-03-24 18:08:37 +0100

    Added ESP32-H2 support (#371)
    * Added ESP32-H2 support
    
    * fix esp32h2.rs (fmt)
    
    * fix mod.rs (fmt)

--- espflash/src/targets/mod.rs
+++ espflash/src/targets/mod.rs
@@ -17,6 +17,7 @@ pub use self::{
     esp32c2::Esp32c2,
     esp32c3::Esp32c3,
     esp32c6::Esp32c6,
+    esp32h2::Esp32h2,
     esp32s2::Esp32s2,
     esp32s3::Esp32s3,
     esp8266::Esp8266,
@@ -34,6 +35,7 @@ mod esp32;
 mod esp32c2;
 mod esp32c3;
 mod esp32c6;
+mod esp32h2;
 mod esp32s2;
 mod esp32s3;
 mod esp8266;
@@ -57,6 +59,8 @@ pub enum Chip {
     Esp32s2,
     /// ESP32-S3
     Esp32s3,
+    /// ESP32-H2
+    Esp32h2,
     /// ESP8266
     Esp8266,
 }
@@ -75,6 +79,8 @@ impl Chip {
             Ok(Chip::Esp32s2)
         } else if Esp32s3::has_magic_value(magic) {
             Ok(Chip::Esp32s3)
+        } else if Esp32h2::has_magic_value(magic) {
+            Ok(Chip::Esp32h2)
         } else if Esp8266::has_magic_value(magic) {
             Ok(Chip::Esp8266)
         } else {
@@ -90,6 +96,7 @@ impl Chip {
             Chip::Esp32c6 => Box::new(Esp32c6),
             Chip::Esp32s2 => Box::new(Esp32s2),
             Chip::Esp32s3 => Box::new(Esp32s3),
+            Chip::Esp32h2 => Box::new(Esp32h2),
             Chip::Esp8266 => Box::new(Esp8266),
         }
     }
