f2f41181d8e90a81a3abbea2cca0bca70d089f48
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-12-20 11:52:17 +0100

    feat: Check that it flashed properly

--- espflash/tests/cli.rs
+++ espflash/tests/cli.rs
@@ -1,6 +1,7 @@
-use assert_cmd::prelude::*; // Add methods on commands
-                            // use predicates::prelude::*; // Used for writing assertions
-use std::process::Command; // Run programs
+use assert_cmd::prelude::*;
+use std::process::{exit, Command};
+use std::thread::sleep;
+use std::time::Duration;
 
 #[test]
 fn flash() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,13 +11,35 @@ fn flash() -> Result<(), Box<dyn std::error::Error>> {
     cmd.arg("board-info");
     cmd.assert().success();
 
-    let mut cmd = Command::cargo_bin("espflash")?;
-
     // flash
     let image = std::env::var("ESPFLASH_APP").expect("ESPFLASH_APP not set");
 
-    cmd.arg("flash").arg(image);
-    cmd.assert().success();
+    let mut child = Command::cargo_bin("espflash")?
+        .args(&["flash", "--monitor", &image])
+        .stdout(std::process::Stdio::piped())
+        // .stderr(std::process::Stdio::piped())
+        .spawn()?;
 
+    // Sleep for 10 seconds
+    sleep(Duration::from_secs(10));
+
+    // Check if the child process is still running
+    match child.try_wait() {
+        Ok(Some(_status)) => {
+            // The process has terminated
+        }
+        Ok(None) => {
+            // The process is still running, kill it
+            child.kill().expect("command wasn't running");
+            child.wait().expect("unable to wait on child");
+        }
+        Err(e) => {
+            panic!("Error attempting to wait on child: {}", e);
+        }
+    }
+
+    let output = child.wait_with_output().expect("Failed to read stdout");
+    let output = String::from_utf8_lossy(&output.stdout).to_string();
+    assert!(output.contains("Hello world!"));
     Ok(())
 }
