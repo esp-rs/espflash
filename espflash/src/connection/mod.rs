1070d1b63916854bf0f9bcd6d632aff52e4716d6
DaneSlattery <bioplus28@gmail.com>
2024-10-02 15:23:24 +0200

    Update download mode detection
    Previously, this failed to detect download mode during this regex match in the following message.
    
    ```
    ESP-ROM:esp32s3-20210327
        Build:Mar 27 2021
        rst:0x1 (POWERON),boot:0x0 (DOWNLOAD(USB/UART0))
        waiting for download
    
    ```
    
    `.*` only matches on any character EXCEPT line breaks,  where as [\s\S]` will match on anything.

--- espflash/src/connection/mod.rs
+++ espflash/src/connection/mod.rs
@@ -173,7 +173,7 @@ impl Connection {
 
             let read_slice = String::from_utf8_lossy(&buff[..read_bytes as usize]).into_owned();
 
-            let pattern = Regex::new(r"boot:(0x[0-9a-fA-F]+)(.*waiting for download)?").unwrap();
+            let pattern = Regex::new(r"boot:(0x[0-9a-fA-F]+)([\s\S]*waiting for download)?").unwrap();
 
             // Search for the pattern in the read data
             if let Some(data) = pattern.captures(&read_slice) {
