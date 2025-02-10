6a40571edde713b69778bd85da6ee6e24b8e8649
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-02-10 16:09:42 +0100

    docs: Add TODOs for issue #697

--- espflash/tests/scripts/erase-flash.sh
+++ espflash/tests/scripts/erase-flash.sh
@@ -5,7 +5,8 @@ echo "$result"
 if [[ ! $result =~ "Flash has been erased!" ]]; then
     exit 1
 fi
-result=$(espflash read-flash 0 0x2000 flash_content.bin 2>&1)
+# TODO: Once https://github.com/esp-rs/espflash/issues/697 is resolved this should read a larger portion of flash
+result=$(espflash read-flash 0 0x200 flash_content.bin 2>&1)
 echo "$result"
 if [[ ! $result =~ "Flash content successfully read and written to" ]]; then
     exit 1
