2ba58ada4e84fc3ea3386ddb96be9b9278aca4b2
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-02-14 16:16:57 +0100

    tests: Add log-format test

--- espflash/tests/scripts/flash.sh
+++ espflash/tests/scripts/flash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $1 2>&1)
+app="/espflash/tests/data/$1"
+result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app 2>&1)
 echo "$result"
 if [[ ! $result =~ "Flashing has completed!" ]]; then
     echo "Flashing failed!"
@@ -9,4 +9,18 @@ fi
 if ! echo "$result" | grep -q "Hello world!"; then
     echo "Monitoring failed!"
     exit 1
+fi
+
+if [[ "$1" == "esp32c6" ]]; then
+    app="${app}_demft"
+    result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app --log-format dfmt 2>&1)
+    echo "$result"
+    if [[ ! $result =~ "Flashing has completed!" ]]; then
+        echo "Flashing failed!"
+        exit 1
+    fi
+    if ! echo "$result" | grep -q "Hello world!"; then
+        echo "Monitoring failed!"
+        exit 1
+    fi
 fi
