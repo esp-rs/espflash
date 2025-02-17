7064b0f892fa75c6d423161cc2e31a9f91eabf74
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-02-17 11:36:14 +0100

    feat: Change test order to keep a non-defmt elf flashed on c6

--- espflash/tests/scripts/flash.sh
+++ espflash/tests/scripts/flash.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 app="espflash/tests/data/$1"
-result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app 2>&1)
-echo "$result"
-if [[ ! $result =~ "Flashing has completed!" ]]; then
-    echo "Flashing failed!"
-    exit 1
-fi
-if ! echo "$result" | grep -q "Hello world!"; then
-    echo "Monitoring failed!"
-    exit 1
-fi
 
 if [[ "$1" == "esp32c6" ]]; then
-    app="${app}_defmt"
-    result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app --log-format defmt 2>&1)
+    app_defmt="${app}_defmt"
+    result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app_defmt --log-format defmt 2>&1)
     echo "$result"
     if [[ ! $result =~ "Flashing has completed!" ]]; then
         echo "Flashing failed!"
@@ -23,4 +13,15 @@ if [[ "$1" == "esp32c6" ]]; then
         echo "Monitoring failed!"
         exit 1
     fi
+fi
+
+result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app 2>&1)
+echo "$result"
+if [[ ! $result =~ "Flashing has completed!" ]]; then
+    echo "Flashing failed!"
+    exit 1
+fi
+if ! echo "$result" | grep -q "Hello world!"; then
+    echo "Monitoring failed!"
+    exit 1
 fi
