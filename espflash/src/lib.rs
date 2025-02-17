576ace1dbcaaf18c9cc0ba86d9dc1c76bc12bfcd
Jesse Braham <jesse@beta7.io>
2025-02-17 08:52:27 +0100

    Don't document the modules/types behind the `cli` feature

--- espflash/src/lib.rs
+++ espflash/src/lib.rs
@@ -41,41 +41,36 @@ pub mod targets;
 mod elf;
 mod error;
 
-/// Command-line interface
+// Command-line interface
 #[cfg(feature = "cli")]
-#[cfg_attr(docsrs, doc(cfg(feature = "cli")))]
 pub mod cli;
 
-/// Logging utilities
+// Logging utilities
 #[cfg(feature = "cli")]
-#[cfg_attr(docsrs, doc(cfg(feature = "cli")))]
 pub mod logging {
-    use env_logger::Env;
+    use env_logger::{Builder, Env};
     use log::LevelFilter;
 
     /// Initialize the logger with the given [LevelFilter]
     pub fn initialize_logger(filter: LevelFilter) {
-        env_logger::Builder::from_env(Env::default().default_filter_or(filter.as_str()))
+        Builder::from_env(Env::default().default_filter_or(filter.as_str()))
             .format_target(false)
             .init();
     }
 }
 
-/// Check for updates
+// Check for updates
 #[cfg(feature = "cli")]
-#[cfg_attr(docsrs, doc(cfg(feature = "cli")))]
 pub mod update {
     use std::time::Duration;
 
     use log::info;
-    use update_informer::{registry, Check};
+    use update_informer::{registry::Crates, Check};
 
-    /// Check crates.io for a new version of the application
     pub fn check_for_update(name: &str, version: &str) {
         // By setting the interval to 0 seconds we invalidate the cache with each
         // invocation and ensure we're getting up-to-date results
-        let informer =
-            update_informer::new(registry::Crates, name, version).interval(Duration::from_secs(0));
+        let informer = update_informer::new(Crates, name, version).interval(Duration::from_secs(0));
 
         if let Some(version) = informer.check_version().ok().flatten() {
             info!("🚀 A new version of {name} is available: {version}");
