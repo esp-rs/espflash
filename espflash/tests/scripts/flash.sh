75710a7672d6edb8a6f1f4bc25c22f9797f47e96
Sergio Gasquez <sergio.gasquez@gmail.com>
2025-02-17 11:26:04 +0100

    feat: Update defmt elf

--- espflash/tests/scripts/flash.sh
+++ espflash/tests/scripts/flash.sh
@@ -13,7 +13,7 @@ fi
 
 if [[ "$1" == "esp32c6" ]]; then
     app="${app}_defmt"
-    result=$(DEFMT_LOG=info timeout 8s espflash flash --no-skip --monitor --non-interactive $app --log-format defmt 2>&1)
+    result=$(timeout 8s espflash flash --no-skip --monitor --non-interactive $app --log-format defmt 2>&1)
     echo "$result"
     if [[ ! $result =~ "Flashing has completed!" ]]; then
         echo "Flashing failed!"
