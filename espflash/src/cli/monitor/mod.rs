e87e392856db72e64efa13c873c927e850746236
Dániel Buga <bugadani@gmail.com>
2025-04-01 10:04:35 +0200

    Undo visibility change

--- espflash/src/cli/monitor/mod.rs
+++ espflash/src/cli/monitor/mod.rs
@@ -39,7 +39,7 @@ pub mod external_processors;
 pub mod parser;
 
 mod line_endings;
-pub(crate) mod symbols;
+mod symbols;
 
 #[cfg_attr(feature = "cli", derive(clap::ValueEnum))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Display, EnumIter, EnumString, VariantNames)]
