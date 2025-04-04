99b5b0624bfa68cd58900b2124719860a839f8aa
Dániel Buga <bugadani@gmail.com>
2025-04-04 14:02:57 +0200

    Test

--- espflash/tests/scripts/write-bin.sh
+++ espflash/tests/scripts/write-bin.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+part_table="espflash/tests/data/partitions.csv"
 
 # https://github.com/esp-rs/espflash/issues/622 reproducer
 echo -ne "\x01\xa0" >binary_file.bin
@@ -10,6 +11,25 @@ if [[ ! $result =~ "Binary successfully written to flash!" ]]; then
 fi
 
 result=$(espflash read-flash 0 64 flash_content.bin 2>&1)
+echo "$result"
+if [[ ! $result =~ "Flash content successfully read and written to" ]]; then
+    echo "Failed to read flash content"
+    exit 1
+fi
+# Check that the flash_content.bin contains the '01 a0' bytes
+if ! grep -q -a -F $'\x01\xa0' flash_content.bin; then
+    echo "Failed verifying content"
+    exit 1
+fi
+
+result=$(espflash write-bin nvs binary_file.bin --partition-table $part_table 2>&1)
+echo "$result"
+if [[ ! $result =~ "Binary successfully written to flash!" ]]; then
+    echo "Failed to write binary to the nvs partition label"
+    exit 1
+fi
+
+result=$(espflash read-flash 0x9000 64 flash_content.bin 2>&1)
 echo "$result"
 if [[ ! $result =~ "Flash content successfully read and written to" ]]; then
     echo "Failed to read flash content"
