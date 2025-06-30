3f808440f49b7b5a1ffafc6acddb8d3645e68b49
Juraj Sadel <juraj.sadel@espressif.com>
2025-06-30 20:12:08 +0200

    help find cargo

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 use clap::Parser;
 
@@ -29,10 +29,24 @@ fn main() -> Result<()> {
         .filter_module("xtask", log::LevelFilter::Info)
         .init();
 
-    // The directory containing the cargo manifest for the 'xtask' package is a
-    // subdirectory within the cargo workspace:
-    let workspace = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let workspace = workspace.parent().unwrap().canonicalize()?;
+    // Determine the path to the workspace (i.e. the root of the repository).
+    // At compile-time we know where the `xtask` crate lives, but that absolute
+    // path may not exist at runtime once the binary is distributed as an
+    // artefact and executed on a different machine (e.g. a self-hosted CI
+    // runner). Therefore we
+    //  1. Try the compile-time location first.
+    //  2. Fallback to the current working directory if that fails.
+
+    let workspace_from_build = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .expect("`CARGO_MANIFEST_DIR` should always have a parent")
+        .to_path_buf();
+
+    let workspace = if workspace_from_build.exists() {
+        workspace_from_build.canonicalize()?
+    } else {
+        env::current_dir()?.canonicalize()?
+    };
 
     match Cli::parse() {
         Cli::GenerateEfuseFields(args) => efuse_generator::generate_efuse_fields(&workspace, args),
