4b7d2bba028414640e9aeaed19657a7cb89b9a06
Kirill Mikhailov <konnor1980@yandex.ru>
2026-07-15 14:49:29 +0200

    fix: regenerate efuse for S31

--- xtask/src/efuse_generator.rs
+++ xtask/src/efuse_generator.rs
@@ -116,7 +116,7 @@ fn parse_efuse_fields(efuse_yaml_path: &Path) -> Result<EfuseFields> {
     //       which can be iterated over, but for now this is good enough.
     const CHIPS: &[&str] = &[
         "esp32", "esp32c2", "esp32c3", "esp32c5", "esp32c6", "esp32c61", "esp32h2", "esp32p4",
-        "esp32s2", "esp32s3",
+        "esp32s2", "esp32s3", "esp32s31",
     ];
 
     let mut efuse_fields = EfuseFields::new();
